559afbbd81c60ab0a4d0b4c6c16a4e8a0ba2991e
mohayonao <mohayonao@gmail.com>
2012-03-19 21:55:45 +0900

    ユーザの操作を追加

--- public/javascripts/mimi.js
+++ public/javascripts/mimi.js
@@ -84,13 +84,22 @@ jQuery(function() {
         var BinaryTrack = function() {
             initialize.apply(this, arguments);
         }, $this = BinaryTrack.prototype;
-
+        
         var initialize = function(sys, options) {
             this.sys = sys;
-            this._signal = new Float32Array(sys.STREAM_FULL_SIZE);
+            this._signal = new Float32Array(128);
             this._func = function(t) { return 0; };
             this._t = 0;
-            this._tstep = 8000 / sys.SAMPLERATE;
+            this._tstepTable = (function() {
+                var dx, list;
+                dx = Math.pow(2, 1/12);
+                list = [];
+                for (i = 0; i <= 24; i++) {
+                    list[i] = 8000 * Math.pow(dx, i-12) / sys.SAMPLERATE;
+                }
+                return list;
+            }());
+            this._tstep = this._tstepTable[12];
             this._list = new Uint8Array(128);
         };
         
@@ -100,22 +109,25 @@ jQuery(function() {
             };
         };
         
+        $this.setTStep = function(v) {
+            if (0 <= v && v < this._tstepTable.length) {
+                this._tstep = this._tstepTable[v|0];
+            }
+        };
+        
         $this.next = function() {
             var signal,func;
             var t, tstep;
-            var i, j, k, v;
+            var i;
             
             signal = this._signal;
             func = this._func;
             t = this._t;
             tstep = this._tstep;
             
-            k = 0;
-            for (i = signal.length / 128; i--; ) {
-                for (j = 0; j < 128; j++) {
-                    signal[k++] = (func(t) & 0x0ff) / 128 - 0.5;
-                    t += tstep;
-                }
+            for (i = 0; i < 128; i++) {
+                signal[i] = (func(t) & 0x0ff) / 128 - 0.5;
+                t += tstep;
             }
             this._t = t;
             
@@ -123,6 +135,91 @@ jQuery(function() {
         };
         
         return BinaryTrack;
+    }());
+
+    var FileStepLoader = (function() {
+        var FileStepLoader = function() {
+            initialize.apply(this, arguments);
+        }, $this = FileStepLoader.prototype;
+
+        var initialize = function(sys, options) {
+            this._readBytes = 1024;
+            this._fetchBytes = 128;
+            
+            this._buffers = [];
+            this._currentBuffer   = null;
+            this._bufferReadIndex = 0;
+            
+            this._file = null;
+            this._fileReadIndex = 0;
+            
+            this._noneArray = new Uint8Array(this._readBytes);
+        };
+        
+        $this.set = function(file, callback) {
+            var self;
+            
+            self = this;
+            this._file = file;
+            this._file.slice = file.webkitSlice || file.mozSlice;
+            this._mutex = 0;
+            
+            this.fileread(function() {
+                self._currentBuffer   = self._buffers.shift() || this._noneArray;
+                self._bufferReadIndex = 0;
+                if (callback) callback();
+            });
+        };
+        
+        $this.fileread = function(callback) {
+            var self;
+            var blob, size, begin, end;
+            var reader;
+            
+            if (this._mutex === 0) {
+                this._mutex = 1;
+                self  = this;
+                size  = this._readBytes;
+                begin = this._fileReadIndex;
+                end   = begin + size;
+                
+                blob = this._file.slice(begin, end);
+                this._fileReadIndex = end;
+                
+                reader = new FileReader();
+                reader.onload = function(e) {
+                    var result, buf, i;
+                    result = e.target.result;
+                    buf = new Uint8Array(size);
+                    for (i = result.length; i--; ) {
+                        buf[i] = result.charCodeAt(i);
+                    }
+                    self._buffers.push(buf);
+                    self._mutex = 0;
+                    if (callback) callback();
+                };
+                reader.readAsBinaryString(blob);
+            }
+        };
+        
+        $this.fetch = function() {
+            var begin, end;
+            var buffer;
+            begin = this._bufferReadIndex;
+            end   = begin + this._fetchBytes;
+            this._bufferReadIndex = end;
+            
+            buffer = this._currentBuffer.subarray(begin, end);
+            if (this._readBytes <= end) {
+                this._currentBuffer   = this._buffers.shift() || this._noneArray;
+                this._bufferReadIndex = 0;
+            } else if (this._buffers.length === 0) {
+                this.fileread();
+            }
+            return buffer;
+        };
+        
+        return FileStepLoader
     }());
     
     
@@ -139,94 +236,73 @@ jQuery(function() {
             
             this._track = new BinaryTrack(sys);
             
-            this._file = null;
-            this._fileReadIndex = 0;
-            this._fileReadCount = 0;
-            
             this._amp = 1.0;
-            this._nullindex = 0;
+            this.reverb = new pico.effects.Reverb(sys, {depth:0.25});
+            this.filter = new pico.effects.IIRFilter(sys, {});
             
-            this.efx = new pico.effects.Reverb(sys, {depth:0.25});
+            this._stepLoader = new FileStepLoader(sys, {});
+            
+            this._mute = false;
+            this._sampleCount = 0;
+            this.setBPM(180);
         };
         
-        $this.play = function(callback) {
-            this._callback = callback || function() {};
-            this.sys.play(this);
+        $this.setBPM = function(bpm) {
+            this._bpm = bpm;
+            this._sampleCountMax = (60 / this._bpm) * this.sys.SAMPLERATE * (4/16);
         };
 
+        $this.setMute = function(mute) {
+            this._mute = mute;
+        };
+        
+        $this.play = function(file) {
+            var self = this;
+            this._stepLoader.set(file, function() {
+                self.sys.play(self);
+                self._fileReadIndex = 0;
+                self._amp = 1.0;
+                self._sampleCount = 0;
+            });
+        };
+        
         $this.stop = function() {
             this.sys.stop();
         };
         
-        $this.set = function(file) {
-            file.slice = file.webkitSlice || file.mozSlice;
-            this._file = file;
-            this._fileReadIndex = 0;
-            this._amp = 1.0;
-            this._nullindex = 0;
-        };
-        
         $this.next = function() {
             var self;
+            var bytes;
             var signal, s;
             var amp;
-            var i;
+            var i, j, k;
             
             self   = this;
             signal = new Float32Array(this.sys.STREAM_FULL_SIZE);
             amp = this._amp * 0.25;
-            
-            s = this._track.next();
-            for (i = s.length; i--; ) {
-                signal[i] += s[i] * amp;
+            if (this._mute) {
+                amp = 0.0;
             }
-            this.efx.process(signal);
             
-            if (this._fileReadCount === 0) {
-                this.fileread(function(list) {
-                    self._track.compose(list);
-                    self.view.set(list);
-                });
-                this._fileReadCount = 1;
-            } else {
-                --this._fileReadCount;
+            k = 0;
+            for (i = signal.length/128; i--; ) {
+                if (this._sampleCount <= 0) {
+                    bytes = this._stepLoader.fetch();
+                    this._track.compose(bytes);
+                    this.view.set(bytes);
+                    this._sampleCount += this._sampleCountMax;
+                }
+                this._sampleCount -= 128;
+                
+                s = this._track.next();
+                for (j = 0; j < 128; j++) {
+                    signal[k++] = s[j] * amp;
+                }
             }
+            this.filter.process(signal);
+            this.reverb.process(signal);
             
             return signal;
-        };
-        
-        $this.fileread = function(callback) {
-            var self;
-            var blob, size, begin, end;
-            var reader, x;
-            
-            self = this;
-            size = this._width;
-            begin = this._fileReadIndex;
-            end   = begin + size;
-            if (this._file.fileSize < end) {
-                end = this._file.fileSize;
-                this._nullindex += 1;
-                this._amp = 1.0 - (this._nullindex / this._height);
-                if (this._amp < 0.0) this._amp = 0.0;
-                if (this._nullindex > this._height) {
-                    if (this._callback) this._callback();
-                }
-            }
-            blob = this._file.slice(begin, end);
-            this._fileReadIndex = end;
-            
-            reader = new FileReader();
-            reader.onload = function(e) {
-                var result, list, i;
-                result = e.target.result;
-                list = new Uint8Array(size);
-                for (i = result.length; i--; ) {
-                    list[i] = result.charCodeAt(i);
-                }
-                callback(list);
-            };
-            reader.readAsBinaryString(blob);
         };
         
         return SoundSystem;
@@ -244,10 +320,7 @@ jQuery(function() {
             e.preventDefault();
             e.stopPropagation();
             
-            sys.set(e.originalEvent.dataTransfer.files[0])
-            sys.play(function() {
-                jQuery("#stop").click();
-            });
+            sys.play(e.originalEvent.dataTransfer.files[0]);
             jQuery("#stop").show();
         });
         
@@ -259,7 +332,144 @@ jQuery(function() {
         jQuery(window).on("resize", function(e) {
             sys.view.resize();
         });
-
+        
         jQuery("#hint").text("drag & drop a file");
+        
+        (function() {
+            var mute = false;
+            var tstep = 12;
+            var reverb = 32;
+            var quit_tap = 0;
+            var bpm = 180;
+            var bpm_tap = 0;
+            var cutoff = 880;
+            var resonance = 0.1;
+            var origin = location.href;
+            var timerId = 0;
+            
+            jQuery(window).on("keydown", function(e) {
+                var result = "";
+                var msec;
+                switch (e.keyCode) {
+
+                //// System
+                case 90: // 'z'
+                    jQuery("#header").toggle("slow");
+                    break;
+                case 81: // 'q'
+                    if (quit_tap === 0) {
+                        quit_tap = +new Date();
+                    } else {
+                        msec = +new Date() - quit_tap;
+                        if (msec < 1250) {
+                            jQuery("#stop").click();
+                            jQuery("#header").show("slow");
+                        } else {
+                            quit_tap = +new Date();
+                        }
+                    }
+                    break;
+                case 88: // 'x'
+                    mute = !mute;
+                    sys.setMute(mute);
+                    result = "mute=" + (mute?"ON":"OFF");
+                    break;
+
+                ///// BPM
+                case 81: // 'q'
+                    bpm -= 1;
+                    if (bpm < 15) bpm = 15;
+                    sys.setBPM(bpm);
+                    result = "bpm=" + bpm;
+                    break;
+                case 87: // 'w'
+                    bpm += 1;
+                    if (480 < bpm) bpm = 480;
+                    sys.setBPM(bpm);
+                    result = "bpm=" + bpm;
+                    break;
+                case 32: // SPC
+                    if (bpm_tap === 0) {
+                        bpm_tap = +new Date();
+                    } else {
+                        msec = +new Date() - bpm_tap;
+                        if (msec > 1250) {
+                            bpm_tap = +new Date();
+                        } else {
+                            bpm_tap = 0;
+                            bpm = (60000/msec)|0;
+                            sys.setBPM(bpm);
+                            result = "bpm=" + bpm;
+                        }
+                    }
+                    break;
+                
+                ///// pitch
+                case 85: // 'u'
+                    tstep -= 1;
+                    if (tstep < 0) tstep = 0;
+                    sys._track.setTStep(tstep);
+                    result = "pitch=" + (tstep-12);
+                    break;
+                case 73: // 'i'
+                    tstep += 1;
+                    if (24 < tstep) tstep = 24;
+                    sys._track.setTStep(tstep);
+                    result = "pitch=" + (tstep-12);
+                    break;
+                
+                ///// Reverb
+                case 79: // 'o'
+                    reverb -= 1;
+                    if (reverb < 0) reverb = 0;
+                    sys.reverb.setDepth(reverb/128);
+                    result = "reverb=" + reverb;
+                    break;
+                case 80: // 'p'
+                    reverb += 1;
+                    if (128 < reverb) reverb = 128;
+                    sys.reverb.setDepth(reverb/128);
+                    result = "reverb=" + reverb;
+                    break;
+                    
+                ///// LowPass Filger
+                case 78: // 'n'
+                    cutoff *= 0.916666;
+                    if (cutoff < 110) cutoff = 110;
+                    sys.filter.setCutoff(cutoff);
+                    result = "cutoff=" + cutoff.toFixed(0);
+                    break;
+                case 77: // 'm'
+                    cutoff *= 1.0833333;
+                    if (14080 < cutoff) cutoff = 14080;
+                    sys.filter.setCutoff(cutoff);
+                    result = "cutoff=" + cutoff.toFixed(0);
+                    break;
+                case 75: // 'l'
+                    resonance -= 0.05;
+                    if (resonance < 0.1) resonance = 0.1;
+                    sys.filter.setResonance(resonance);
+                    result = "resonance=" + resonance.toFixed(2);
+                    break;
+                case 76: // 'k'
+                    resonance += 0.05;
+                    if (1.0 < resonance) resonance = 1.0;
+                    sys.filter.setResonance(resonance);
+                    result = "resonance=" + resonance.toFixed(2);
+                    break;
+                }
+                
+                if (result) {
+                    history.pushState(null, null, origin + '?' + result);
+                    if (timerId !== 0) {
+                        clearTimeout(timerId);
+                    }
+                    timerId = setTimeout(function() {
+                        history.pushState(null, null, origin);
+                        timerId = 0;
+                    }, 2000);
+                }
+            });
+        }());
     }
 });
