3ccaa45ac7ed2f354bb48e5372c470ed2c0d4484
mohayonao <mohayonao@gmail.com>
2012-04-07 22:33:25 +0900

    command追加

--- public/javascripts/perfume0.js
+++ public/javascripts/perfume0.js
@@ -20,92 +20,229 @@ window.onload = function() {
     renderer.setClearColorHex(0x0000ff, 0.2);
 	container.appendChild(renderer.domElement);
     
-    MotionMan.prototype.init = function() {
+    MotionMan.prototype.init = function(color) {
         var children, o, i, imax;
         var geometry, line;
+        
+        this.position.set(0, 170, 0);
         
         children = this.children;
         for (i = 0, imax = children.length; i < imax; i++) {
             o = children[i];
-            o.material.color = new THREE.Color(0xffffff);
+            o.material.color = new THREE.Color(color);
             
             if (o.$parent) {
                 geometry = new THREE.Geometry();
                 geometry.vertices.push(new THREE.Vertex(o.position));
                 geometry.vertices.push(new THREE.Vertex(o.$parent.position));
                 line = new THREE.Line(geometry, new THREE.LineBasicMaterial(
-                    {color:0xffffff, linewidth:5, opacity:0.5}));
+                    {color:color, linewidth:5, opacity:0.5}));
                 
                 this.add(line);
             }
         }
+        
+        this.frameBegin = 0;
+        this.frameEnd   = 2820;
+        this.frameStep  = 1;
+        this.muted = false;
     };
-
-    var frameBegin, frameEnd, frameStep;
-    frameBegin =    0;
-    frameEnd   = 2820;
-    frameStep  =    1;
     
-    var K = new MotionMan(scene);
+    var A = new MotionMan();
+    var K = new MotionMan();
+    var N = new MotionMan();
+    var Perfume  = [A, K, N];
     
-    scene.add(K);
+    var $msg = jQuery("#message");
     
-    K.load("/data/spring-of-life-02.bvh", {unmoving:true}, function() {
-        K.init();
-        animate();
+    $msg.text("aachan loading...");
+    A.load("/data/spring-of-life-01.bvh", {unmoving:true}, function() {
+        A.init(0xff9933);
+        A.name = "aachan";
+        scene.add(A);
+        
+        $msg.text("kashiyuka loading...");
+        K.load("/data/spring-of-life-02.bvh", {unmoving:true}, function() {
+            K.init(0x3399ff);
+            K.name = "kashiyuka";
+            scene.add(K);
+            K.position.x = 200;
+            
+            $msg.text("nocchi loading...");
+            N.load("/data/spring-of-life-03.bvh", {unmoving:true}, function() {
+                N.init(0x33ff99);
+                N.name = "nocchi";
+                N.position.x = -200;
+                scene.add(N);
+                
+                initUI();
+            });
+        });
     });
-    K.position.set(0, 170, 0);
-    
-    var animate = (function() {
-        var begin, time, tx;
-        begin = +new Date();
-        time  = 0;
-        return function animate() {
-            var frame;
-            
-            time = +new Date() - begin;
-            time %= K.totalTime;
-            
-            if (frameBegin === frameEnd) {
-                frame = frameBegin;
-            } else {
-                frame = (time / K.frameTime * frameStep)|0;
-                frame %= (frameEnd - frameBegin);
-                frame += frameBegin;
-            }
-            time = frame * K.frameTime;
-            
-            K.update(time);
-		    renderer.render(scene, camera);
-            
-            requestAnimationFrame(animate);
-   	    };
-    }());
     
     
-    //// UI
-    (function() {
-        var t1;
-        t1 = jQuery(document.createElement("input"))
-            .val(frameBegin + ".." + frameEnd)
-            .css("position", "absolute").css("top", "10px").css("left", "10px")
+    var initUI = function() {
+        var t;
+        $msg.remove();
+        t = jQuery(document.createElement("input"))
+            .css("position", "absolute").css("top", "0px").css("left", "0px")
+            .css("font-size", "2em").css("font-weight", "bold")
+            .css("border", "solid 0px").attr("placeholder", "command")
+            .width("100%")
             .on("change", function(e) {
-                var m, v1, v2, v3;
-                m = /^(\d+)(?:\.\.(\d+)(?:;([.0-9]+))?)?$/.exec(t1.val().trim());
-                if (m) {
-                    t1.css("color", "black");
-                    v1 = m[1]|0;
-                    v2 = (m[2]|0) || v1;
-                    v3 = +m[3];
-                    if (isNaN(v3)) v3 = 1;
-                    else if (v3 < 0) v3 = 0;
-                    if (v2 < v1) v2 = v1;
-                    frameBegin = v1;
-                    frameEnd   = v2;
-                    frameStep = v3;
+                var cmd = t.val().trim();
+                if (doCommand(cmd)) {
+                    t.css("color", "black");
+                    t.val("");
+                    console.log(cmd);
                 } else {
-                    t1.css("color", "red");
+                    t.css("color", "red");
                 }
             }).appendTo(document.body);
-    }());
+        animate();
+    };
+    
+    
+    var doCommand = function(cmd) {
+        var m, func;
+        if (cmd === "help") {
+            // TODO: show command list
+            return true;
+        } else if ((m = /^(?:([aknc]|aachan|kashiyuka|nocchi|cam|camera)\s+)?(.+)$/.exec(cmd)) !== null) {
+            if ((m[1]||"")[0] === "c") {
+                func = functionSelectorForCamera(m[2]);
+                if (func) return func() || true; 
+            } else {
+                func = functionSelectorForPerfume(m[2]);
+                if (func) {
+                    return ({a:[A],k:[K],n:[N]}[(m[1]||"")[0]] || [A,K,N]).forEach(function(q) {
+                        func(q);
+                    }) || true;
+                }
+            }
+        }
+        return false;
+    };
+    
+    var fetchXYZ = function(src, cmd, func) {
+        var m, items, xyz = {x:src.x, y:src.y, z:src.z}, duration = 500;
+        if ((m = cmd.indexOf(";")) !== -1) {
+            duration = cmd.substr(m+1)|0;
+            cmd = cmd.substr(0, m);
+        }
+        
+        if ((m = /^([xyz])\s*([-+]?\d*)$/.exec(cmd)) !== null) {
+            xyz[m[1]] = func ? func(m[2]|0) : m[2]|0;
+        } else {
+            items = cmd.split(",");
+            if (items.length === 1) {
+                xyz.x = xyz.y = xyz.z = func ? func(items[0]||0) : items[0]||0;
+            } else if (items.length === 3) {
+                if (func) {
+                    xyz.x = func(items[0]||0);
+                    xyz.y = func(items[1]||0);
+                    xyz.z = func(items[2]||0);
+                } else {
+                    xyz.x = items[0]||0;
+                    xyz.y = items[1]||0;
+                    xyz.z = items[2]||0;
+                }
+            }
+        }
+        return {xyz:xyz, duration:duration};
+    };
+    
+    
+    var functionSelectorForPerfume = function(cmd) {
+        var m, xyz, items;
+        
+        if ((m = /^(\d+)(?:\.\.(\d+)(?:;([.0-9]+))?)?$/.exec(cmd)) !== null) {
+            items = [m[1]|0, (m[2]|0)||m[1]|0, +m[3] ];
+            if (isNaN(items[2])) items[2] = 1;
+            else if (items[2] < 0) items[2] = 0;
+            if (items[1] < items[0]) items[1] = items[0];
+            
+            return function(q) {
+                q.frameBegin = items[0];
+                q.frameEnd   = items[1];
+                q.frameStep  = items[2];
+            };
+        } else {
+            items = cmd.split(/\s+/);
+            if (items[0] === "pos" || items[0] === "position") {
+                items = items.slice(1).join(" ");
+                return function(q) {
+                    var to = fetchXYZ(q.position, items);
+                    new TWEEN.Tween(q.position)
+                        .to(to.xyz, to.duration)
+                        .easing(TWEEN.Easing.Cubic.EaseOut)
+                        .start();
+                };
+            } else if (items[0] === "rot" || items[0] === "rotation") {
+                items = items.slice(1).join(" ");
+                return function(q) {
+                    var to = fetchXYZ(q.rotation, items, function(x) {
+                        return (x|0) / 180 * Math.PI;
+                    });
+                    new TWEEN.Tween(q.rotation)
+                        .to(to.xyz, to.duration)
+                        .easing(TWEEN.Easing.Cubic.EaseOut)
+                        .start();
+                };
+            } else if (items[0] === "scale") {
+                items = items.slice(1).join(" ");
+                return function(q) {
+                    var to = fetchXYZ(q.position, items);
+                    new TWEEN.Tween(q.scale)
+                        .to(to.xyz, to.duration)
+                        .easing(TWEEN.Easing.Cubic.EaseOut)
+                        .start();
+                };
+            }
+        }
+    };
+    
+    var functionSelectorForCamera = function(cmd) {
+        var items = cmd.split(/\s+/);
+        if (items[0] === "pos" || items[0] === "position") {
+            items = items.slice(1).join(" ");
+            return function() {
+                var to = fetchXYZ(camera.position, items);
+                console.log("camera", camera.position);
+                new TWEEN.Tween(camera.position)
+                    .to(to.xyz, to.duration)
+                    .easing(TWEEN.Easing.Cubic.EaseOut)
+                    .start();
+            };
+        }
+    };
+    
+    
+    var begin = +new Date();
+    var animate = function() {
+        var time, frame;
+        var i, q;
+        
+        time = +new Date() - begin;
+        time %= A.totalTime;
+        
+        for (i = 0; i < 3; i++) {
+            q = Perfume[i];
+            
+            if (q.frameBegin === q.frameEnd) {
+                frame = q.frameBegin;
+            } else {
+                frame = (time / q.frameTime * q.frameStep)|0;
+                frame %= (q.frameEnd - q.frameBegin);
+                frame += q.frameBegin;
+            }
+            frame *= q.frameTime;
+            q.update(frame);
+        }
+        
+		TWEEN.update();
+        
+		renderer.render(scene, camera);
+        requestAnimationFrame(animate);
+   	};
 };
